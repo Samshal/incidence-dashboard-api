e92b4c98faa5b19906b285ec0c670fc43049809d
samshal <samueladeshina73@gmail.com>
2021-04-15 13:11:48 +0100

    update

--- src/Incidents/Incident/Incident.php
+++ src/Incidents/Incident/Incident.php
@@ -36,15 +36,32 @@ class Incident {
 
         $query = "INSERT INTO Incidents_Incidents(IncidentName, IncidentType, IncidentLocation, IncidentDate, IncidentPointOfInterest, IncidentDescription) VALUES ('$name', $type, $location, '$date', ST_GeomFromText('$pointOfInterest'), '$description');";
 
-        $result = DBConnectionFactory::getConnection()->exec($query);
+        $connection = DBConnectionFactory::getConnection();
+        $result = $connection->exec($query);
+
+        $incidentId = $connection->lastInsertId();
+        if (count($metadata) > 0){
+            $meta = [];
+            foreach($metadata as $key=>$value){
+                $meta[] = "($incidentId, $key, '$value')";
+            }
+
+            $query = "INSERT INTO Incidents_IncidentMetadata (IncidentId, FieldId, FieldValue) VALUES ".implode(",", $meta);
+            $result = $connection->exec($query);
+        }
         
-        return $result;
+        return ["lastInsertId"=>$incidentId];
     }
 
     public static function viewIncidents(){
         $query = "SELECT a.IncidentId, a.IncidentName, a.IncidentDate, ST_asText(a.IncidentPointOfInterest) as IncidentPointOfInterest, b.*, c.* FROM Incidents_Incidents a INNER JOIN Incidents_IncidentTypes b ON a.IncidentType = b.IncidentTypeId INNER JOIN SpatialEntities_Entities c ON a.IncidentLocation = c.EntityId";
 
         $result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+        foreach($result as $key=>$data){
+            $query = "SELECT * FROM Incidents_IncidentMetadata a INNER JOIN Incidents_MetadataFields b ON a.FieldId = b.FieldId WHERE a.IncidentId = ".$data['IncidentId'];
+            $_result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+            $result[$key]["metadata"] = $_result;
+        }
 
         return $result;
     }
@@ -52,7 +69,11 @@ class Incident {
     public static function viewIncident(int $resourceId){
         $query = "SELECT a.IncidentId, a.IncidentName, a.IncidentDate, ST_asText(a.IncidentPointOfInterest) as IncidentPointOfInterest, b.*, c.* FROM Incidents_Incidents a INNER JOIN Incidents_IncidentTypes b ON a.IncidentType = b.IncidentTypeId INNER JOIN SpatialEntities_Entities c ON a.IncidentLocation = c.EntityId WHERE a.IncidentId = $resourceId";
         $result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
-
+        foreach($result as $key=>$data){
+            $query = "SELECT * FROM Incidents_IncidentMetadata a INNER JOIN Incidents_MetadataFields b ON a.FieldId = b.FieldId WHERE a.IncidentId = ".$data['IncidentId'];
+            $_result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+            $result[$key]["metadata"] = $_result;
+        }
         return $result;
     }
 }
